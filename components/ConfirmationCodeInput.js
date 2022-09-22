7c6398268ed37a57812a6b9164ce8441c43de983
Garry Laly <garrylaly@gmail.com>
2022-09-22 14:20:42 +0700

    Change TextInput.propTypes.style to PropTypes.object

--- components/ConfirmationCodeInput.js
+++ components/ConfirmationCodeInput.js
@@ -16,7 +16,7 @@ export default class ConfirmationCodeInput extends Component {
     inactiveColor: PropTypes.string,
     ignoreCase: PropTypes.bool,
     autoFocus: PropTypes.bool,
-    codeInputStyle: TextInput.propTypes.style,
+    codeInputStyle: PropTypes.object,
     containerStyle: PropTypes.object,
     onFulfill: PropTypes.func,
     onCodeChange: PropTypes.func,
